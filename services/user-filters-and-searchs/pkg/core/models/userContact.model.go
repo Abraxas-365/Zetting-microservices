5a57d20e1dfe05c548179fce8676ec7c60eafa03
Luis Fernando Miranda <abraxas@abraxas-3.local>
2022-05-10 16:37:44 -0500

    depracated

--- services/user-filters-and-searchs/pkg/core/models/userContact.model.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Contact struct {
-	Email                   string `json:"email"`
-	Phone                   string `json:"phone,omitempty"`
-	Country                 string `json:"country,omitempty"`
-	IdentifierDocumentation string `json:"identifier_document,omitempty"`
-}
--- services/user-filters-and-searchs/pkg/core/ports/repository.port.go
+++
@@ -1,8 +0,0 @@
-package ports
-
-import "user/pkg/core/models"
-
-type UserRepository interface {
-	GetUsersByProfession(profession string, page int) (models.Users, error)
-	GetUsersByProject(projectId interface{}, document string) (models.Users, error)
-}
