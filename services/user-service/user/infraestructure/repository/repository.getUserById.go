489e6580290deda979b6b6119124c66f6e79326c
Luis Fernando Miranda <abraxas@abraxas-3.local>
2022-05-14 14:11:37 -0500

    replacing hardcoded collection

--- services/user-service/user/infraestructure/repository/repository.getUserById.go
+++ services/user-service/user/infraestructure/repository/repository.getUserById.go
@@ -11,7 +11,7 @@ import (
 func (r mongoRepository) GetUserById(userId uuid.UUID) (models.User, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), r.timeout)
 	defer cancel()
-	collection := r.client.Database(r.database).Collection("Users")
+	collection := r.client.Database(r.database).Collection(r.collection)
 	var user models.User
 	filter := bson.M{"_id": userId}
 	if err := collection.FindOne(ctx, filter).Decode(&user); err != nil {
